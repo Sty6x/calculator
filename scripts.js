d1e12f8f940007b0af812a4212afbdb9ef72de4a
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-05-03 10:39:42 +0400

    able to log each buttons with their respective numbers

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Operations
 const add = (a,b) => a + b
 const subtract = (a,b) => a - b
 const multiply = (a,b) => a * b
@@ -7,4 +8,25 @@ function operator(operation, a ,b){
     return operation(a,b)
 }
 
-console.log(operator(division,8,2))
+
+
+//display text
+
+
+let button1 = document.querySelector('.one');
+let button2 = document.querySelector('.two');
+let buttons = document.querySelectorAll('.buttons')
+const display = document.getElementById('display');
+
+// console.log(buttons)
+
+let arrButtons = Array.from(buttons);
+
+// need to check if value is not a number and change
+// the values into numbers instead of string
+const getInputNumbers = arrButtons.map(button => {
+    button.addEventListener('click',() =>{
+    console.log(+button.value)
+    return button.value;})
+})
+
