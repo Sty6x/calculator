bb6524f926916a395a773dc01454bc154d413944
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-05-03 10:56:43 +0400

    can now display numbers on calculator

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
 // Operations
-const add = (a,b) => a + b
-const subtract = (a,b) => a - b
-const multiply = (a,b) => a * b
-const division = (a,b) => a / b
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const division = (a, b) => a / b
 
-function operator(operation, a ,b){
-    return operation(a,b)
+function operator(operation, a, b) {
+    return operation(a, b)
 }
-
-
+let newarr = [];
 
 //display text
 
-
-let button1 = document.querySelector('.one');
-let button2 = document.querySelector('.two');
 let buttons = document.querySelectorAll('.buttons')
 const display = document.getElementById('display');
 
@@ -22,11 +18,14 @@ const display = document.getElementById('display');
 
 let arrButtons = Array.from(buttons);
 
-// need to check if value is not a number and change
-// the values into numbers instead of string
-const getInputNumbers = arrButtons.map(button => {
-    button.addEventListener('click',() =>{
-    console.log(+button.value)
-    return button.value;})
+const getInputNumberDisplay = arrButtons.map(button => {
+    button.addEventListener('click', () => {
+        newarr.push(button.value);
+        console.log(newarr)
+        let displayText = [];
+        for (let i = 0; i < newarr.length; i++) {
+            displayText += newarr[i];
+        }
+        display.textContent = displayText
+    })
 })
-
