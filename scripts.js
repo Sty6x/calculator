76520292a4b679aa8b296c055f01174bd19b4679
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-05-04 13:02:58 +0400

    modified script for better understanding

--- scripts.js
+++ scripts.js
@@ -18,76 +18,37 @@ let arrButtons = Array.from(buttons);
 // storedValues is now modified and can be used with the operations
 let storedValues = [];
 
+
 // MODIFYING OUR storedValues ARRAY
+arrButtons.forEach(getDisplayValues)
 function getDisplayValues(buttons) {
     buttons.addEventListener('click', () => {
         storedValues.push(buttons.value)
-        console.log(storedValues)
+        console.log("stored values array:" + storedValues)
     })
 }
-arrButtons.forEach(getDisplayValues)
 // MODIFYING OUR storedValues ARRAY
 
 
 
-
-// concatenate the elements of the and displays the value
-// on our calculator display
-function displayText() {
-    arrButtons.forEach(buttons => {
-        buttons.addEventListener('click', () => {
-            let displayValues = '';
-            for (let i = 0; i < storedValues.length; i++) {
-                displayValues += storedValues[i];
-            }
-            display.textContent = displayValues
-        })
-    })
+function concatValues() {
+    let displayValues = '';
+    for (let i = 0; i < storedValues.length; i++) {
+        displayValues += storedValues[i];
+    }
+    return displayValues;
 }
-displayText();
-// concatenate the elements of the and displays the value
+
+// on our calculator display
+const displayText = (concatenate) => {display.textContent = concatenate}
 // on our calculator display
 
 
 
-// console logging values
-// arrButtons.forEach(buttons => {
-//     buttons.addEventListener('click', () => {
-//         console.log("storedValues: " + storedValues)
+arrButtons.forEach(buttons => {
+    buttons.addEventListener('click', () => {
+        console.log("display values returned: "+ concatValues())
+        displayText(concatValues());
 
-//     })
-// })
-
-
-function calculate() {
-    arrButtons.forEach(buttons => {
-        buttons.addEventListener('click', () => {
-            let displayValues = '';    
-            for (let i = 0; i < storedValues.length; i++) {
-                displayValues += storedValues[i];
-                switch (storedValues[i]) {
-                    // if button is "+"
-                    case '+':
-                        // stores the first value
-                        let aValue = displayValues
-                        console.log("first Value: " + aValue)
-                        break;
-                        // if button is "-"
-                    case '-':
-                        console.log('subtraction')
-                        break;
-                        // if button is "x" 
-                    case 'x':
-                        console.log('multiplication')
-                        break;
-                        // if button is "/"
-                    case '/':
-                        console.log('division')
-                        break;
-                }
-            }
-        })
     })
-}
-
-calculate()
+})
