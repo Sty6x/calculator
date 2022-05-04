5043495efa543d5b6d4d72776bb7c0c1ba36d85f
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-05-04 09:04:16 +0400

    added comments for better understanding of the function

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const division = (a, b) => a / b
 function operator(operation, a, b) {
     return operation(a, b)
 }
-let newarr = [];
 
 //display text
 
@@ -16,16 +15,38 @@ const display = document.getElementById('display');
 
 // console.log(buttons)
 
-let arrButtons = Array.from(buttons);
 
-const getInputNumberDisplay = arrButtons.map(button => {
+let arrButtons = Array.from(buttons);
+let newarr = [];
+
+// we want to make a reusable function that returns the numbers inputted
+
+
+function getNumberInput(button) {
+    // this method activates a function on every 'click'
+    // of the user
     button.addEventListener('click', () => {
+
+        // pushing button.value to newarr array
+        // on every click
         newarr.push(button.value);
-        console.log(newarr)
-        let displayText = [];
+
+        // concactinating all the numbers inputted on the array
+        // to a new variable for us to display it on the browser
+        // called displayText (a string type) 
+        // can be converted to number type using unary operator "+"
+        let displayText = '';
         for (let i = 0; i < newarr.length; i++) {
             displayText += newarr[i];
         }
+
+        // blitting the displaytext on our display 
+        // on the calculator
         display.textContent = displayText
+        console.table(displayText)
     })
-})
+}
+
+// .map applies a function(getNumberInput) to each button. 
+const getInputNumberDisplay = arrButtons.map(getNumberInput)
+
