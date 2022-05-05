e515dd9bd52d8f23eb72991b172c008edcfbc190
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-05-05 09:39:36 +0400

    added a clear functionality to calculator

--- scripts.js
+++ scripts.js
@@ -12,7 +12,8 @@ function operator(operation, a, b) {
 
 let buttons = document.querySelectorAll('.buttons')
 const display = document.getElementById('display');
-
+let clearButton = document.querySelector('.clear')
+console.log(clearButton)
 // converts nodelist into array Array.from
 let arrButtons = Array.from(buttons);
 // storedValues is now modified and can be used with the operations
@@ -21,6 +22,7 @@ let storedValues = [];
 
 // MODIFYING OUR storedValues ARRAY
 arrButtons.forEach(getDisplayValues)
+
 function getDisplayValues(buttons) {
     buttons.addEventListener('click', () => {
         storedValues.push(buttons.value)
@@ -28,6 +30,19 @@ function getDisplayValues(buttons) {
     })
 }
 // MODIFYING OUR storedValues ARRAY
+
+
+function clearValues() {
+    let i = 0
+    while (i < storedValues.length){
+        i++
+        console.log(storedValues.pop())
+        console.log(storedValues)
+
+    }
+    display.textContent = [];
+}
+clearButton.addEventListener('click', clearValues)
 
 
 
@@ -39,16 +54,21 @@ function concatValues() {
     return displayValues;
 }
 
+
 // on our calculator display
-const displayText = (concatenate) => {display.textContent = concatenate}
+const displayText = (concatenate) => {
+    display.textContent = concatenate
+}
 // on our calculator display
 
 
 
 arrButtons.forEach(buttons => {
     buttons.addEventListener('click', () => {
-        console.log("display values returned: "+ concatValues())
+        console.log("display values returned: " + concatValues())
         displayText(concatValues());
 
     })
 })
+
+
